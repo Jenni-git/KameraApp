467ee9f5f45648b9db9d5cc68afdff1c75980520
jenni <37399577+Jenni-git@users.noreply.github.com>
2018-08-23 10:31:45 +0300

    aa

---
+++ MainActivity.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Graphics;
+using Android.Content;
+using System.Collections.Generic;
+using System.IO;
+using Android.Provider;
+using Android.Content.PM;
+
+namespace CameraApp
+{
+
+    public static class App
+    {
+        public static File _file;
+        public static File _dir;
+        public static Bitmap bitmap;
+    }
+
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+	public class MainActivity : AppCompatActivity
+	{
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+
+            if (IsThereAnAppToTakePictures())
+            {
+                CreateDirectoryForPictures();
+
+                Button button = FindViewById<Button>(Resource.Id.myButton);
+                _imageView = FindViewById<ImageView>(Resource.Id.imageView1);
+                button.Click += TakeAPicture;
+            }
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void FabOnClick(object sender, EventArgs eventArgs)
+        {
+            View view = (View) sender;
+            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
+                .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
+        }
+
+        private void CreateDirectoryForPictures()
+        {
+            App._dir = new File(
+                System.Environment.GetExternalStoragePublicDirectory(
+                    System.Environment.DirectoryPictures), "CameraAppDemo");
+            if (!App._dir.Exists())
+            {
+                App._dir.Mkdirs();
+            }
+        }
+
+        private bool IsThereAnAppToTakePictures()
+        {
+            Intent intent = new Intent(MediaStore.ActionImageCapture);
+            IList<ResolveInfo> availableActivities =
+                PackageManager.QueryIntentActivities(intent, PackageInfoFlags.MatchDefaultOnly);
+            return availableActivities != null && availableActivities.Count > 0;
+        }
+
+        private void TakeAPicture(object sender, EventArgs eventArgs)
+        {
+            Intent intent = new Intent(MediaStore.ActionImageCapture);
+            App._file = new File(App._dir, String.Format("myPhoto_{0}.jpg", Guid.NewGuid()));
+            intent.PutExtra(MediaStore.ExtraOutput, Uri.FromFile(App._file));
+            StartActivityForResult(intent, 0);
+        }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+
+            // Make it available in the gallery
+
+            Intent mediaScanIntent = new Intent(Intent.ActionMediaScannerScanFile);
+            Uri contentUri = Uri.FromFile(App._file);
+            mediaScanIntent.SetData(contentUri);
+            SendBroadcast(mediaScanIntent);
+
+            // Display in ImageView. We will resize the bitmap to fit the display.
+            // Loading the full sized image will consume to much memory
+            // and cause the application to crash.
+
+            int height = Resources.DisplayMetrics.HeightPixels;
+            int width = _imageView.Height;
+            App.bitmap = App._file.Path.LoadAndResizeBitmap(width, height);
+            if (App.bitmap != null)
+            {
+                _imageView.SetImageBitmap(App.bitmap);
+                App.bitmap = null;
+            }
+
+            // Dispose of the Java side bitmap.
+            GC.Collect();
+        }
+
+        public static class BitmapHelpers
+        {
+            public static Bitmap LoadAndResizeBitmap(this string fileName, int width, int height)
+            {
+                // First we get the the dimensions of the file on disk
+                BitmapFactory.Options options = new BitmapFactory.Options { InJustDecodeBounds = true };
+                BitmapFactory.DecodeFile(fileName, options);
+
+                // Next we calculate the ratio that we need to resize the image by
+                // to fit the requested dimensions.
+                int outHeight = options.OutHeight;
+                int outWidth = options.OutWidth;
+                int inSampleSize = 1;
+
+                if (outHeight > height || outWidth > width)
+                {
+                    inSampleSize = outWidth > outHeight
+                                       ? outHeight / height
+                                       : outWidth / width;
+                }
+
+                // Now we will load the image and have BitmapFactory resize it for us.
+                options.InSampleSize = inSampleSize;
+                options.InJustDecodeBounds = false;
+                Bitmap resizedBitmap = BitmapFactory.DecodeFile(fileName, options);
+
+                return resizedBitmap;
+            }
+        }
+    }
+    
+
+}
+
